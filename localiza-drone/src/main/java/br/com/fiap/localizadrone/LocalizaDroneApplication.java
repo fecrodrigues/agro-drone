c228ca3ec785a4e02b174151ca5fa4beefa94a5f
Breno Marino de Souza Ferreira <breno_marino@hotmail.com>
2020-11-02 22:25:58 -0300

    configuracao swagger e outros ajustes

--- localiza-drone/src/main/java/br/com/fiap/localizadrone/LocalizaDroneApplication.java
+++ localiza-drone/src/main/java/br/com/fiap/localizadrone/LocalizaDroneApplication.java
@@ -2,8 +2,10 @@ package br.com.fiap.localizadrone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("br.com.fiap")
 public class LocalizaDroneApplication {
 
 	public static void main(String[] args) {
